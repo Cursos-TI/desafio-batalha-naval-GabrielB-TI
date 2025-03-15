a01d305dcba85f445141257bba4cb095a70c0fd3
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-03-15 18:08:43 +0000

    posiccionamento dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,17 @@ int main() {
         }
     }
 
+    int linha1 = 2, coluna1 = 4; 
+    int linha2 = 6, coluna2 = 1; 
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha1][coluna1 + i] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha2 + i][coluna2] = 3;
+    }
+
     printf("\nTabuleiro de Batalha Naval:\n");
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 10; y++) {
