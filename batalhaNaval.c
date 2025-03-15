71e90214f7956701458c1b92cc0948e6bea1b3ae
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-03-15 18:01:36 +0000

    Correçao do printf

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,19 +2,21 @@
 
 int main() {
 
-    int tabuleiro [10][10];
+    int tabuleiro[10][10]; 
 
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 10; y++) {
             tabuleiro[x][y] = 0;
         }
     }
-   
-    printf("\n TABULEIRO BATALHA NAVAL \n");
+
+    printf("\nTabuleiro de Batalha Naval:\n");
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 10; y++) {
-            printf("%d", tabuleiro[x][y]);
+            printf("%d ", tabuleiro[x][y]);
         }
-    
+        printf("\n");
+    }
+
     return 0;
 }
